ae3c0cbc56048fb865763757d0b7d331b00254c3
Zunrong <838180986@qq.com>
2019-08-17 18:15:05 +0800

    2019-08-17 18:14

--- src/ArrayProblem.java
+++ src/ArrayProblem.java
@@ -4,9 +4,27 @@ import java.util.Stack;
 public class ArrayProblem {
 
 
-
     // 找出数组中每个数右边第一个比它大的元素
-//    //
+    //  1、暴力遍历
+    //我们很容易想到复杂度为O(n^2)的解法，遍历数组中的每一个后面所有元素，找到第一个大于它的，输出即可。
+    public int[] findMaxRight(int[] array) {
+        if (array == null) {
+            return array;
+        }
+        int size = array.length;
+        int[] result = new int[size];
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (array[j] > array[i]) {
+                    result[i] = array[j];
+                    break;
+                }
+            }
+        }
+        result[size - 1] = -1;//最后一个元素右边没有元素，所以肯定为-1
+        return result;
+    }
+//    // 2.借助栈，时间复杂度O(n)
 //    步骤如下：
 //
 //     （1）初始化栈，里面为第一个元素索引0值；
@@ -19,6 +37,8 @@ public class ArrayProblem {
 //           3）如果当前遍历的元素值A[i]小于栈顶元素索引的值A[stack.peek()]，说明还未找到栈顶元素中右边第一个比它大的元素，
 //           直接将当前遍历的元素的索引入栈即可stack.push(i); 将i++，重复步骤（2）
 //      （3）直到遍历完所有元素，如果栈不为空，说明栈中保存的全是未找到右边第一个比它大的数组索引，我们依次将这些栈元素出栈，并赋值result[stack.pop()]=-1即可。
+
+
     public int[] findMaxRightWithStack(int[] arr) {
         if (arr == null) {
             return arr;
@@ -41,4 +61,33 @@ public class ArrayProblem {
         }
         return result;
     }
+
+    //LeetCode 1013 将数组分成和相等的三个部分
+//    给定一个整数数组 A，只有我们可以将其划分为三个和相等的非空部分时才返回 true，否则返回 false。
+//
+//形式上，如果我们可以找出索引 i+1 < j 且满足 
+// (A[0] + A[1] + ... + A[i] == A[i+1] + A[i+2] + ... + A[j-1] == A[j] + A[j-1] + ... + A[A.length - 1]) 就可以将数组三等分。
+
+
+    public boolean canThreePartsEqualSum(int[] A) {
+        int sum = 0;
+        for (int a : A) {
+            sum += a;
+        }
+        int key = sum / 3;
+        int temp = 0;
+        for (int i = 0; i < A.length; i++) {
+            key -= A[i];
+            if (key == 0) {
+                temp++;
+                key = sum / 3;
+            }
+        }
+        if (temp == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
