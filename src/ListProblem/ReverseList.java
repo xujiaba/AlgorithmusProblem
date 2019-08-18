a7ce22b568351d749cad9ad4036a4ab904ec8d9b
Zunrong <838180986@qq.com>
2019-08-18 09:20:19 +0800

    添加题-反转链表中的一段

---
+++ src/ListProblem/ReverseList.java
@@ -0,0 +1,37 @@
+package ListProblem;
+
+public class ReverseList {
+
+    //LeetCode 92 反转链表中的一段
+    public ListNode reversePart(ListNode head, int from, int to) {
+        int len = 0;
+        ListNode node1 = head;
+        ListNode fPre = null;
+        ListNode tPos = null;
+        while (node1 != null) {
+            len++;
+            fPre = len == from - 1 ? node1 : fPre;
+            tPos = len == to + 1 ? node1 : tPos;
+            node1 = node1.next;
+        }
+        if (from > to || from < 1 || to > len) {
+            return head;
+        }
+        node1 = fPre == null ? head : fPre.next;
+        ListNode node2 = node1.next;
+        node1.next = tPos;
+        ListNode next = null;
+        while (node2 != tPos){
+            next = node2.next;
+            node2.next = node1;
+            node1 = node2;
+            node2 = next;
+        }
+        if (fPre != null){
+            fPre.next = node1;
+            return head;
+        }
+        return node1;
+
+    }
+}
